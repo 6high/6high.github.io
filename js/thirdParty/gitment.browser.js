e2e721559e9b8cae58c6cb8d213461f3adff25a0
6high <gao.liu.cn@gmail.com>
2018-04-01 20:43:07 +0800

    Site updated: 2018-04-01 20:43:07

--- js/thirdParty/gitment.browser.js
+++ js/thirdParty/gitment.browser.js
@@ -4963,7 +4963,7 @@ var Gitment = function () {
     this.defaultTheme = _default2.default;
     (0, _assign2.default)(this, {
     //   id: window.location.href,
-      id: window.location.pathname,
+      id: window.document.title,
       title: window.document.title,
       link: window.location.href,
       desc: '',
