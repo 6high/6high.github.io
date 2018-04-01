65933510cd5b11bf392a94e0e147f85bad438519
6high <gao.liu.cn@gmail.com>
2018-04-01 20:13:57 +0800

    Site updated: 2018-04-01 20:13:57

--- js/thirdParty/gitment.browser.js
+++ js/thirdParty/gitment.browser.js
@@ -5091,7 +5091,8 @@ var Gitment = function () {
 
       return _utils.http.post('/repos/' + owner + '/' + repo + '/issues', {
         title: title,
-        labels: labels,
+        // labels: labels,
+        labels: title,
         body: link + '\n\n' + desc
       }).then(function (meta) {
         _this5.state.meta = meta;
